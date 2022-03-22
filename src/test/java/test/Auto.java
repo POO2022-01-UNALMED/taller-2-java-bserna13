41ba494edbe621fcb8f60afa8a099fdf29efa133
bserna13 <101461524+bserna13@users.noreply.github.com>
2022-03-22 00:10:51 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -10,21 +10,29 @@ public class Auto {
     static int cantidadCreados;
 
     int cantidadAsientos(){
-        return 2;
+        int contador=0;
+        for(int i=0;i<asientos.length;i++){
+            if (asientos[i]!=null){
+                contador++;
+            }
+        }
+        return contador;
     }
 
     String verificarIntegridad(){
-        
-        for( int i=0;i<asientos.length;i++){
-            if((asientos[0]).registro!= (asientos[i]).registro){
-                return "Las piezas no son originales";      
+        for(int i=0;i<asientos.length;i++){
+            if (asientos[i]!=null){
+                if(((asientos[i]).registro==registro) & (motor.registro==registro) & ((asientos[i]).registro==motor.registro)){
+                    return "Auto original";
+                }
+                return "Las piezas no son originales";
             }
         }
-
-        if(((asientos[0]).registro==registro) & (motor.registro==registro) & ((asientos[0]).registro==motor.registro)){
-            return "Auto original";
-        }
         return "Las piezas no son originales";
+        
+        
+        
+        
     }
 
 
